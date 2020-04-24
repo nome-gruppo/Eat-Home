15521eab6f85647da5b21be5b28c444e138d3ab8
symonsg <symon_99@outlook.it>
2020-04-24 22:38:01 +0200

    bugfix

--- eathome/src/main/java/nomeGruppo/eathome/actions/Feedback.java
+++ eathome/src/main/java/nomeGruppo/eathome/actions/Feedback.java
@@ -5,34 +5,25 @@ import java.util.Date;
 
 public class Feedback {
 
-    private int idFeedback; //TODO rendi final
-    private int voteFeedback;
-    private String titleFeedback;
-    private String textFeedback;
-    private Date dateFeedback;
-    private String answerFeedback;
-    private final FeedbackType feedbackTypeFeedback;
-    private final int idClientFeedback;
-    private final int idPlaceFeedback;
+    public String idFeedback;
+    public int voteFeedback;
+    public String textFeedback;
+    public Date dateFeedback;
+    public String feedbackTypeFeedback; //vedi FeedbackType
+    public String idClientFeedback;
+    public String idPlaceFeedback;
 
-    public Feedback(int vote, FeedbackType feedbackType, int idClient, int idPlace){
-        //TODO leggi l'ultimo id dal database e incrementa uno
-        this.voteFeedback = vote;
-        this.dateFeedback = Calendar.getInstance().getTime();
-        this.feedbackTypeFeedback = feedbackType;
-        this.idClientFeedback = idClient;
-        this.idPlaceFeedback = idPlace;
+
+    public Feedback() {
     }
 
-    public void setText(String text) {
-        this.textFeedback = text;
-    }
-
-    public void setTitle(String title) {
-        this.titleFeedback = title;
-    }
-
-    public void setAnswer(String answer) {
-        this.answerFeedback = answer;
+    public Feedback(String idFeedback, int voteFeedback, String textFeedback, Date dateFeedback, String feedbackTypeFeedback, String idClientFeedback, String idPlaceFeedback) {
+        this.idFeedback = idFeedback;
+        this.voteFeedback = voteFeedback;
+        this.textFeedback = textFeedback;
+        this.dateFeedback = dateFeedback;
+        this.feedbackTypeFeedback = feedbackTypeFeedback;
+        this.idClientFeedback = idClientFeedback;
+        this.idPlaceFeedback = idPlaceFeedback;
     }
 }
