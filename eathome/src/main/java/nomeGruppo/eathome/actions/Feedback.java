3bd36c1e7de61b352e8645865cfda7a5b74f074b
symonsg <symon_99@outlook.it>
2020-04-25 10:54:21 +0200

    bugfix

--- eathome/src/main/java/nomeGruppo/eathome/actions/Feedback.java
+++ eathome/src/main/java/nomeGruppo/eathome/actions/Feedback.java
@@ -8,8 +8,7 @@ public class Feedback {
     public String idFeedback;
     public int voteFeedback;
     public String textFeedback;
-    public Date dateFeedback;
-    public String feedbackTypeFeedback; //vedi FeedbackType
+    public String dateFeedback;
     public String idClientFeedback;
     public String idPlaceFeedback;
 
@@ -17,12 +16,11 @@ public class Feedback {
     public Feedback() {
     }
 
-    public Feedback(String idFeedback, int voteFeedback, String textFeedback, Date dateFeedback, String feedbackTypeFeedback, String idClientFeedback, String idPlaceFeedback) {
+    public Feedback(String idFeedback, int voteFeedback, String textFeedback, String dateFeedback, String idClientFeedback, String idPlaceFeedback) {
         this.idFeedback = idFeedback;
         this.voteFeedback = voteFeedback;
         this.textFeedback = textFeedback;
         this.dateFeedback = dateFeedback;
-        this.feedbackTypeFeedback = feedbackTypeFeedback;
         this.idClientFeedback = idClientFeedback;
         this.idPlaceFeedback = idPlaceFeedback;
     }
